c20022db971823ca467858f556c4cddeabdeac10
Rachelnarios <Rnr266@Nyu.edu>
2018-11-20 19:04:28 -0500

    fixed indentation

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const path = require('path')
 var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.urlencoded({ extended: false }))
-  .use(function(req, res, next) { next() })
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'hbs')
-
-  .get('/', (req, res) => res.render('land'))
-
-
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+.use(express.static(path.join(__dirname, 'public')))
+.use(bodyParser.urlencoded({ extended: false }))
+.use(function(req, res, next) { next() })
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+.get('/', (req, res) => res.render('land'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
