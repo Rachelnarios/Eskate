1136f3f6dd5a570cf3170d8c6a68ac567cb282f6
SummerLi <xl1029@nyu.edu>
2018-12-05 18:28:14 -0500

    Feat
    Added a simple web map page for some station markers

---
+++ js/dbhelper.js
@@ -0,0 +1,31 @@
+/**
+ * Common database helper functions.
+ */
+class DBHelper {
+
+  /**
+   * Database URL.
+   * Change this to stations.json file location on your server.
+   */
+  static get DATABASE_URL() {
+    const port = 8000 // Change this to your server port
+    return `http://localhost:${port}/data/stations.json`;
+  }
+
+
+
+  /**
+   * Map marker for a station.
+   */
+   static mapMarkerForStation(station, map) {
+    // https://leafletjs.com/reference-1.3.0.html#marker  
+    const marker = new L.marker([station.latlng.lat, station.latlng.lng],
+      {title: station.name,
+      alt: station.name,
+      })
+      marker.addTo(newMap);
+    return marker;
+  } 
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,54 @@
+let bikes, stations;
+var newMap;
+var marker;
+
+/**
+ * Fetch neighborhoods and cuisines as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {
+  initMap(); // added 
+});
+
+
+
+/**
+ * Initialize leaflet map, called from HTML.
+ */
+initMap = () => {
+  self.newMap = L.map('map', {
+        center: [40.72925325, -73.99625393609625],
+        zoom: 14,
+        scrollWheelZoom: false
+      });
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    mapboxToken: 'pk.eyJ1Ijoic3VtbWVyLWxpIiwiYSI6ImNqb2E5Ynd2cDFweW4zcW5sYXZkN3Z0d3gifQ.2FiNxbrZXlfFlHQNrPXjRA',
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(newMap);
+  self.marker = L.marker([40.735863, -73.991084]).addTo(newMap);
+  self.marker.bindPopup("<b>Union Square!</b><br>Skateboard Station.").openPopup();
+  //updateStations();
+}
+
+
+
+
+/**
+ * Add markers for current stations to the map.
+ */
+addMarkersToMap = (stations = self.stations) => {
+  stations.forEach(station => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForStation(station, self.newMap);
+    marker.on("click", onClick);
+    function onClick() {
+      window.location.href = marker.options.url;
+    }
+    self.markers.push(marker);
+  });
+} 
+
+
